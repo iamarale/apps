120aef666708895521f97f0111342a98a9ca86c0
iamarale <ismailarales@gmail.com>
2024-05-13 03:40:25 -0500

    style: changed logo to darker shade of blue

--- quoting/src/components/Header.tsx
+++ quoting/src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className=" bg-neutral-200 text-neutral-900 text-center py-6 px-2 mb-12 md:py-12">
       <h1 className="text-2xl font-bold uppercase">
-        Quo<span className="text-blue-400">ting</span>
+        Quo<span className="text-blue-600">ting</span>
       </h1>
       <h3>
         Get a variety of quotes ranging from forgiveness, inspirational,
